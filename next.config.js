5e880a47fb899d89b46dde96c02c0334d958c0c5
ChiragChrg <chiruchirag2001@gmail.com>
2023-07-23 23:30:41 +0530

    Installed Next-Pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+})
+
+const nextConfig = withPWA({
     images: {
         domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"]
     },
-}
+})
+
 
 module.exports = nextConfig
