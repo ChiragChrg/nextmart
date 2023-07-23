7303e9ea42ad4f6be251534d4b04c8c79011a17a
ChiragChrg <chiruchirag2001@gmail.com>
2023-07-23 23:36:14 +0530

    Added PWA and other metatags

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,30 @@ import Provider from './Provider'
 export const metadata: Metadata = {
   title: 'NextMart | Next Generation Ecommerce Market',
   description: 'NextMart is an Ecommerce website',
+  creator: "ChiragChrg",
+  authors: [{ name: 'ChiragChrg', url: 'https://chiragchrg.netlify.app/' }],
+  verification: { google: "YRAUUyc8TP4QJ1s53KAdLxcON9xifQf33BnLeP-F_5Y" },
+  applicationName: "Next Mart",
+  manifest: "/manifest.json",
+  keywords: ['Next Mart', 'next-mart', 'nextmart', 'chiragchrg', 'ChiragChrg', 'chiruchirag2001', 'ecommerce', 'shopping', 'next.js'],
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#FAFAFA' },
+    { media: '(prefers-color-scheme: dark)', color: '#050505' },
+  ],
+  icons: {
+    icon: '/icons/192.png',
+    shortcut: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+  },
 }
 
 export default function RootLayout({
