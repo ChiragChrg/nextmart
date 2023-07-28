29f37b8d237600caa494c9e3afa5df28039a1277
ChiragChrg <chiruchirag2001@gmail.com>
2023-07-29 02:37:00 +0530

    Minor Fixes

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,8 +6,9 @@ import Search from "./Search"
 import UserAvatar from "./UserAvatar"
 import LogoSVG from "./SVGs/LogoSVG"
 import IconSVG from "./SVGs/IconSVG"
+import Link from "next/link"
 
-const Header = () => {
+const Header = ({ hideSearch = false }: { hideSearch?: boolean }) => {
     const [isMobile, setIsMobile] = useState<boolean>(false)
     useEffect(() => {
         setIsMobile(window?.innerWidth <= 750)
@@ -17,28 +18,60 @@ const Header = () => {
         return (
             <>
                 <header className="bg-baseClr w-full flex justify-between items-center px-4 py-3 pb-1">
-                    <IconSVG width="35px" />
-                    <UserAvatar />
+                    <Link href="/">
+                        <IconSVG width="30px" />
+                    </Link>
+                    {!hideSearch && <UserAvatar />}
+
+                    {hideSearch && <div className="flex gap-4 items-center">
+                        <ThemeButton />
+                        <Link href="/" className='bg-primaryClr px-2 py-1 text-white rounded flex_center gap-2'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+                            </svg>
+
+                            <span>Back</span>
+                        </Link>
+                    </div>}
                 </header>
 
-                <div className="sticky top-0 z-10 w-full flex_center px-4 py-2 bg-gradient-to-b from-baseClr backdrop-blur-[1px]">
-                    <Search hideSearchBtn />
-                </div>
+                {!hideSearch &&
+                    <div className="sticky top-0 z-10 w-full flex_center px-4 py-2 bg-gradient-to-b from-baseClr backdrop-blur-[1px]">
+                        <Search hideSearchBtn />
+                    </div>
+                }
             </>
         )
     else
         return (
             <header className="bg-baseClr sticky top-0 z-10 w-full flex justify-between items-center p-3 px-4">
-                <LogoSVG width="200px" />
+                <Link href="/">
+                    <LogoSVG width="200px" />
+                </Link>
 
-                <Search />
+                {!hideSearch &&
+                    <>
+                        <Search />
 
-                <div className="flex gap-3 items-center">
-                    <ThemeButton className='header_btn' width="25px" height="25px" />
-                    <OrdersButton className='header_btn' width="25px" height="25px" />
-                    <CartButton className='header_btn' width="25px" height="25px" />
-                    <UserAvatar />
-                </div>
+                        <div className="flex gap-3 items-center">
+                            <ThemeButton className='header_btn' width="25px" height="25px" />
+                            <OrdersButton className='header_btn' width="25px" height="25px" />
+                            <CartButton className='header_btn' width="25px" height="25px" />
+                            <UserAvatar />
+                        </div>
+                    </>
+                }
+
+                {hideSearch && <div className="flex gap-4 items-center">
+                    <ThemeButton />
+                    <Link href="/" className='bg-primaryClr px-2 py-1 text-white rounded flex_center gap-2'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+                        </svg>
+
+                        <span>Back</span>
+                    </Link>
+                </div>}
             </header>
         )
 }
