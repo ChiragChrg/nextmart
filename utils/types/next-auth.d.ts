c4dd001e86c3bff6fdf64acc599be6a2fe88a00f
ChiragChrg <chiruchirag2001@gmail.com>
2023-07-29 17:08:15 +0530

    Fixed Type Bug Ig

--- utils/types/next-auth.d.ts
+++ utils/types/next-auth.d.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 
 declare module "next-auth" {
     interface User {
+        id?: string | null,
         uid?: string | null,
         name?: string | null,
         email?: string | null,
