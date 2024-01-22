cc063a4ebd38225ce6218fb7ea54ffd362c68635
ChiragChrg <chiruchirag2001@gmail.com>
2024-01-22 20:34:37 +0530

    Updated Meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,69 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import { Metadata } from "next";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
 
-export const metadata = {
+export const metadata: Metadata = {
+  title: 'NextMart | Your Ultimate Shopping Destination!',
+  description: 'Discover an unparalleled shopping experience with Next Mart, an exceptional web app developed by ChiragChrg. Unleash the power of online shopping with a vast array of products at your fingertips',
+  keywords: ["chiragchrg", "chirag", "chrgchirag", 'Next Mart', 'next-mart', 'nextmart', "shopping", "ecommerce", "chiruchirag2001"],
+  authors: [{ name: "ChiragChrg" }, { url: "https://chiragchrg.netlify.app/" }],
+  creator: "ChiragChrg",
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
-};
+  applicationName: "Next Mart",
+  alternates: {
+    canonical: '/',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  verification: {
+    google: 'd2pS_qzLYMcFTx9l8FtCPjCaAQygUYw_NLlTZ_zlDJU',
+  },
+  manifest: "/manifest.json",
+  icons: {
+    icon: '/Icons/144.png',
+    shortcut: '/favicon.svg',
+    apple: '/Icons/192.png',
+  },
+  openGraph: {
+    title: 'NextMart | Your Ultimate Shopping Destination!',
+    description: 'Discover an unparalleled shopping experience with Next Mart, an exceptional web app developed by ChiragChrg. Unleash the power of online shopping with a vast array of products at your fingertips',
+    url: defaultUrl,
+    siteName: 'Next Mart',
+    images: [
+      {
+        url: '/icons/192.png',
+        width: 192,
+        height: 192,
+        alt: 'NextMart Logo',
+      },
+      {
+        url: '/icons/ARMSDevices.png',
+        width: 1800,
+        height: 760,
+        alt: 'NextMart Mockup Preview',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'NextMart | Your Ultimate Shopping Destination!',
+    description: 'Discover an unparalleled shopping experience with Next Mart, an exceptional web app developed by ChiragChrg. Unleash the power of online shopping with a vast array of products at your fingertips',
+    creator: '@chrgchirag',
+    images: ['/icons/192.png', '/icons/ARMSDevices.png'],
+  },
+}
 
 export default function RootLayout({
   children,
