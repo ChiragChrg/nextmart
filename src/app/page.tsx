9895a25f825272b08b26a3e39998da00767e9b23
ChiragChrg <chiruchirag2001@gmail.com>
2024-01-24 23:16:41 +0530

    Initilized Landing page Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Header from '@/components/Headers/Header'
 import React from 'react'
 
 const page = () => {
     return (
         <main className="min-h-screen flex flex-col items-center">
+            <Header />
             <div>page</div>
         </main>
     )
