bc55360bb8ca9da2dc6e75ed60806e2ae26edd21
ChiragChrg <chiruchirag2001@gmail.com>
2024-01-28 20:38:58 +0530

    Adding User session to global store in UserAvatar Component

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -7,36 +7,36 @@ import Image from "next/image"
 import LoaderIcon from "./CustomUI/LoaderIcon"
 import { createClient } from "@/utils/supabase/client"
 import useUserStore from "@/store/useUserStore"
-import { LogoSVG } from "@/assets"
 import { User2Icon } from "lucide-react"
+import toast from "react-hot-toast"
 
 const UserAvatar = () => {
+    const [loading, setLoading] = useState<boolean>(true)
+    const { user, setUser } = useUserStore()
     const supabase = createClient()
-    const { setUser } = useUserStore()
-
-    const [showDropMenu, setShowDropMenu] = useState<boolean>(false)
-    const [loading, setLoading] = useState<string>("loading")
-    const DropdownRef = useRef<HTMLDivElement>(null)
-
     const pathname = usePathname()
     const router = useRouter()
 
-
     useEffect(() => {
         const GetSession = async () => {
-            const { data } = await supabase.auth.getSession()
+            const { data, error } = await supabase.auth.getSession()
             const userSession = {
                 uid: data?.session?.user?.id as string,
-                username: data?.session?.user?.user_metadata?.full_name as string,
+                username: data?.session?.user?.user_metadata?.username as string,
                 email: data?.session?.user?.email as string,
                 avatarImg: data?.session?.user?.user_metadata?.avatar_url as string,
                 isAuthenticated: data?.session?.user?.aud ? true : false,
             }
 
+            if (error) {
+                return toast.error(error.message)
+            }
+
             setUser(userSession)
+            setLoading(false)
             console.log("ClientUser", data)
         }
-        // GetSession()
+        GetSession()
     }, [setUser, supabase.auth])
 
     const HandleLogout = async () => {
@@ -49,15 +49,15 @@ const UserAvatar = () => {
         }
     }
 
-    if (loading === "authenticated") {
+    if (user) {
         return (
             <>
                 {/* Desktop User Avatar */}
                 <div className="hidden lg:flex justify-center items-center relative w-fit gap-2 cursor-pointer hover:bg-secondaryClr border border-secondaryClr p-1 px-2 rounded smooth_transition" >
-                    {true ?
+                    {user?.avatarImg ?
                         <div className="flex_center rounded-full relative overflow-hidden">
-                            <Image src={LogoSVG} alt="ProfileImage" width={35} height={35} />
-                        </div >
+                            <Image src={user?.avatarImg} alt="ProfileImage" width={35} height={35} />
+                        </div>
                         :
                         <div className="bg-primaryClr aspect-square text-white p-1 rounded-full">
                             <User2Icon size={35} />
@@ -66,7 +66,7 @@ const UserAvatar = () => {
 
                     <div className="flex flex-col justify-center max-w-fit w-full ml-1 overflow-hidden">
                         <span className="text-[0.75em] leading-[1em] text-textLiteClr">Welcome !</span>
-                        <span className="text-[0.9em]">TempUser</span>
+                        <span className="text-[0.9em]">{user?.username}</span>
                     </div>
 
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-textLiteClr">
@@ -76,9 +76,9 @@ const UserAvatar = () => {
 
                 {/* Mobile User Avatar */}
                 <Link href="/profile" className="lg:hidden">
-                    {true ?
+                    {user?.avatarImg ?
                         <div className="flex_center rounded-full relative overflow-hidden" >
-                            <Image src={LogoSVG} alt="ProfileImage" width={40} height={40} />
+                            <Image src={user?.avatarImg} alt="ProfileImage" width={40} height={40} />
                         </div>
                         :
                         <div className="bg-primaryClr aspect-square text-white p-1 rounded-full" >
@@ -92,8 +92,8 @@ const UserAvatar = () => {
     else {
         return (
             <>
-                {loading !== "loading" ?
-                    <Link href="/login" onClick={() => setLoading("loading")} className="bg-primaryClr flex_center gap-2 text-white px-2 py-[0.3em] rounded cursor-pointer">
+                {!loading ?
+                    <Link href="/login" className="bg-primaryClr flex_center gap-2 text-white px-2 py-[0.3em] rounded cursor-pointer">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             width="32"
