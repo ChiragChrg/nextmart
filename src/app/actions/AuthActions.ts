e568b754aef1f7436862748185c6c5cb2e2ec562
ChiragChrg <chiruchirag2001@gmail.com>
2024-01-28 20:16:25 +0530

    Login and OAuth login using supabase server actions done

--- src/app/actions/AuthActions.ts
+++ src/app/actions/AuthActions.ts
@@ -31,3 +31,70 @@ export const registerUser = async ({ username, email, password }: RegisterProps)
 
     return authResponse
 };
+
+type LoginProps = {
+    email: string,
+    password: string,
+}
+
+export const loginUser = async ({ email, password }: LoginProps) => {
+    if (!email || !password) {
+        throw new Error("Missing Fields")
+    }
+
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+
+    const authResponse = await supabase.auth.signInWithPassword({
+        email,
+        password,
+    });
+
+    return authResponse
+};
+
+type Provider =
+    | 'apple'
+    | 'azure'
+    | 'bitbucket'
+    | 'discord'
+    | 'facebook'
+    | 'figma'
+    | 'github'
+    | 'gitlab'
+    | 'google'
+    | 'kakao'
+    | 'keycloak'
+    | 'linkedin'
+    | 'linkedin_oidc'
+    | 'notion'
+    | 'slack'
+    | 'spotify'
+    | 'twitch'
+    | 'twitter'
+    | 'workos'
+    | 'zoom'
+    | 'fly'
+
+export const OAuthLogin = async (provider: Provider) => {
+    if (!provider) {
+        throw new Error("Missing Provider")
+    }
+
+    const origin = headers().get("origin");
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+
+    const authResponse = await supabase.auth.signInWithOAuth({
+        provider: provider,
+        options: {
+            queryParams: {
+                access_type: 'offline',
+                prompt: 'consent',
+            },
+            redirectTo: `${origin}/auth/callback`,
+        }
+    })
+
+    return authResponse
+};
