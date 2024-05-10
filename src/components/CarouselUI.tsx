be6b655c2fecc2089dde9281f9950819b9d7dcda
ChiragChrg <chiruchirag2001@gmail.com>
2024-05-10 22:45:06 +0530

    Modified Carousel UI

--- src/components/CarouselUI.tsx
+++ src/components/CarouselUI.tsx
@@ -7,6 +7,52 @@ import {
 } from "@/components/ui/carousel"
 
 export const CarouselUI = () => {
+    type carouselType = {
+        title: string,
+        textA: string,
+        textB: string,
+        poster: string
+    }[]
+
+    const mockCarousel: carouselType = [
+        {
+            title: "Halwa",
+            textA: "The Brand New Halwa!",
+            textB: "Savour every bite.",
+            poster: "http://via.placeholder.com/600x400"
+        },
+        {
+            title: "Laddu",
+            textA: "The Brand New Laddu!",
+            textB: "Savour every bite.",
+            poster: "http://via.placeholder.com/600x400"
+        },
+        {
+            title: "Ganja",
+            textA: "The Brand New Ganja!",
+            textB: "Savour every bite.",
+            poster: "http://via.placeholder.com/600x400"
+        },
+        {
+            title: "Laptop",
+            textA: "The Brand New Laptop!",
+            textB: "Savour every Moment.",
+            poster: "http://via.placeholder.com/600x400"
+        },
+        {
+            title: "Dress",
+            textA: "The Brand New Dress!",
+            textB: "Savour every Style.",
+            poster: "http://via.placeholder.com/600x400"
+        },
+        {
+            title: "Shoes",
+            textA: "The Brand New Shoes!",
+            textB: "Savour every Step.",
+            poster: "http://via.placeholder.com/600x400"
+        }
+    ]
+
     return (
         // <div className="bg-red-500 w-full max-w-sm">
         <Carousel
@@ -14,11 +60,16 @@ export const CarouselUI = () => {
                 loop: true,
                 align: "center"
             }}
-            className="bg-red-500/30 w-full max-w-sm lg:max-w-[80%]">
+            className="bg-red-500/30 w-full max-w-sm lg:max-w-[90%]">
             <CarouselContent>
-                <CarouselItem>Hello</CarouselItem>
-                <CarouselItem>Bro</CarouselItem>
-                <CarouselItem>Wassup</CarouselItem>
+                {mockCarousel.map((item, index) => (
+                    <CarouselItem key={index} className="p-4 pl-8">
+                        <div
+                            className="bg-primaryClr w-full h-full min-h-[500px] p-4 rounded-md">
+                            <span className='hidden tablet:block text-[1em] font-bold tracking-wider'>{item?.title}</span>
+                        </div>
+                    </CarouselItem>
+                ))}
             </CarouselContent>
             <CarouselPrevious />
             <CarouselNext />
