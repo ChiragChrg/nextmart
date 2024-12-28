ebc847b35c3dc40968110ec091cdf944d6dbf633
ChiragChrg <chiruchirag2001@gmail.com>
2024-12-28 16:54:13 +0530

    style : Added Poppins Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,25 @@
 import "./globals.css";
 import { Metadata } from "next";
 import Provider from "@/providers/Provider";
+import localFont from "next/font/local"
+
+const poppins = localFont({
+  src: [
+    {
+      path: "../assets/fonts/Poppins-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Poppins-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  preload: true,
+  display: "swap",
+  variable: "--Poppins",
+});
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -72,7 +91,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-background text-foreground" suppressHydrationWarning>
+      <body className={`bg-background text-foreground ${poppins.variable} font-poppins`} suppressHydrationWarning>
         <Provider>
           {children}
         </Provider>
