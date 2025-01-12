e36683515605b138b639bfa81ada4a3fbc733eab
ChiragChrg <chiruchirag2001@gmail.com>
2025-01-12 20:52:49 +0530

    chore: specified env variable type

--- src/app/api/payment/createOrderId/route.ts
+++ src/app/api/payment/createOrderId/route.ts
@@ -8,7 +8,7 @@ type RazorOrderType = {
 };
 
 const razorpay = new Razorpay({
-    key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID!,
+    key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID as string,
     key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
